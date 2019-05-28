15b0150aac702c0f6ea9141f036d8b1c159b2a12
Israel Merljak <israel@dbsystem.com.br>
2019-05-28 10:00:06 -0300

    adicionando mensagens ao ConsultaDocumentoTransporteEspecifico.java

--- src/main/java/br/gov/siscomex/portalunico/cct/consultardocumentotransporteespecifico/ConsultaDocumentoTransporteEspecifico.java
+++ src/main/java/br/gov/siscomex/portalunico/cct/consultardocumentotransporteespecifico/ConsultaDocumentoTransporteEspecifico.java
@@ -1,8 +1,11 @@
 
 package br.gov.siscomex.portalunico.cct.consultardocumentotransporteespecifico;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+
+import br.gov.siscomex.portalunico.cct.consultardocumentotransporte.Mensagem;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -12,6 +15,10 @@ public class ConsultaDocumentoTransporteEspecifico {
     @Expose
     private Set<DocumentosTransporte> documentosTransporte = null;
 
+    @SerializedName("mensagens")
+    @Expose
+    private List<Mensagem> mensagens;
+
     public Set<DocumentosTransporte> getDocumentosTransporte() {
         return documentosTransporte;
     }
@@ -20,23 +27,33 @@ public class ConsultaDocumentoTransporteEspecifico {
         this.documentosTransporte = documentosTransporte;
     }
 
+    public List<Mensagem> getMensagens() {
+        return mensagens;
+    }
+
+    public void setMensagens(List<Mensagem> mensagens) {
+        this.mensagens = mensagens;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ConsultaDocumentoTransporteEspecifico that = (ConsultaDocumentoTransporteEspecifico) o;
-        return Objects.equals(documentosTransporte, that.documentosTransporte);
+        return Objects.equals(documentosTransporte, that.documentosTransporte) &&
+                Objects.equals(mensagens, that.mensagens);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(documentosTransporte);
+        return Objects.hash(documentosTransporte, mensagens);
     }
 
     @Override
     public String toString() {
         return "ConsultaDocumentoTransporteEspecifico{" +
                 "documentosTransporte=" + documentosTransporte +
+                ", mensagens=" + mensagens +
                 '}';
     }
 }
